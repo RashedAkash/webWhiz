302d0821191855596d1ab553d0d59c5690d3bd71
hablu <131282586+hablu@users.noreply.github.com>
2023-12-14 08:59:26 +0600

    Updates

---
+++ services.data.js
@@ -0,0 +1,22 @@
+export const services =[
+  {
+    "image": "https://i.postimg.cc/1tZqFSj9/color-sw-atch.png",
+    "title": "JavaScript",
+    "description": "JavaScript (JS) is a high-level, interpreted programming language primarily used for web development. Initially developed by Netscape, it has become one of the most popular and versatile programming languages. JavaScript allows developers to add interactive and dynamic elements to websites, enabling client-side scripting for web applications."
+  },
+  {
+    "image": "https://i.postimg.cc/28pS8GgD/command.png",
+    "title": "React js",
+    "description": "React is an open-source JavaScript library developed by Facebook that is used for building user interfaces, especially for single-page applications where data changes over time without requiring the page to refresh. It was first released in 2013 and has since gained immense popularity in the web development community."
+  },
+  {
+    "image": "https://i.postimg.cc/k46ww7nL/pen-tool.png",
+    "title": "Next js",
+    "description": "Next.js is a popular open-source React framework created by Vercel (formerly known as ZEIT) for building web applications. It is designed to make React-based web development more efficient by providing a comprehensive solution with built-in features that simplify the creation of production-ready React applications."
+  },
+  {
+    "image": "https://i.postimg.cc/28pS8GgD/command.png",
+    "title": "Node js",
+    "description": "Node.js is a powerful open-source, server-side JavaScript runtime environment built on Chrome's V8 JavaScript engine. It allows developers to run JavaScript code outside a web browser, enabling the creation of scalable, high-performance network applications."
+  }
+]
